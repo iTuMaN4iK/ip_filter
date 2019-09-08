990b66d3ef72aab02165e567d10a5b582d1b2cc2
Dima <dima@debian.pc>
2019-09-08 22:27:46 +0500

    travis + CMakeList

---
+++ build/CMakeFiles/vers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vers.dir/lib.cpp.o"
+  "libvers.pdb"
+  "libvers.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/vers.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libvers.a"
+)
