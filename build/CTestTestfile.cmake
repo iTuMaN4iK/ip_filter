cda3a689629b363b8424b437c906f3b8ba0be8b7
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2019-09-09 21:15:09 +0500

    add .gitignore and test deploy Gtest [2]

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dima/workspace/ip_filter
-# Build directory: /home/dima/workspace/ip_filter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
